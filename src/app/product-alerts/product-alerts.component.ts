8a9b31671a9a5b8227e85b849ce303080e011a06
Saif-Haq <itssaifhaq@gmail.com>
2021-09-22 16:33:15 +0500

    4 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -9,7 +9,9 @@ import { Product } from '../products';
   styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent implements OnInit {
-  @Input() product!: Product;
+  //@Input() product!: Product;
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
   constructor() {}
 
   ngOnInit() {}
